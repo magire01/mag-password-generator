bf4caeecd87b5f76aafae68db84c97a859491a86
Mark Gire <magire01@gmail.com>
2020-06-28 17:08:52 -0400

    Created for loop to generate 10 random chars from pwCharacter array

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// pwCharacter is an array with all possible password character options
 var pwCharacter = "abcdeftghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()".split("");
 
+
 console.log(pwCharacter);
+for (var i = 0; i<10; i++) {
+  console.log("PW Character: " + pwCharacter[Math.floor(Math.random() * (Math.floor(72) - Math.ceil(0) + 1))]);
+}
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+// for loop to determine pw size
+  for (var i = 0; i<10; i++) {
+    console.log("PW Character: " + pwCharacter[i]);
+  }
   passwordText.value = password;
 
 }
