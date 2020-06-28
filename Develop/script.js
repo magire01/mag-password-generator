c4219adf2cef5704ad2acee4ebdd9bc1dd6c1029
Mark Gire <magire01@gmail.com>
2020-06-28 17:33:51 -0400

    Changed variable pwString to pwArray and created pwString variable to print out the pwArray into 1 string

--- Develop/script.js
+++ Develop/script.js
@@ -2,35 +2,41 @@
 var generateBtn = document.querySelector("#generate");
 // pwCharacter is an array with all possible password character options
 var pwCharacter = "abcdeftghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()".split("");
-// pwString is an empty arry that will be filled with the for loop
-var pwString = []
+// pwArray is an empty arry that will be filled with the for loop
+var pwArray = []
 //pw Character test
 console.log(pwCharacter);
 
 
 function generatePassword() {
-  // for loop to push 10 random characters to pwString array
+  // for loop to push 10 random characters to pwArray
   for (var i = 0; i<10; i++) {
     //push 10 random characters to pwString array
-    pwString.push(pwCharacter[Math.floor(Math.random() * (Math.floor(72) - Math.ceil(0) + 1))]);
+    pwArray.push(pwCharacter[Math.floor(Math.random() * (Math.floor(72) - Math.ceil(0) + 1))]);
     
   }
-  //pwString test
-  console.log(pwString);
+  //pwArray test
+  console.log(pwArray);
 }
 
 //generatePassword test
 generatePassword();
+// variable to assign the pwArray to a single string
+var pwString = pwArray[0] + pwArray[1] + pwArray[2] + pwArray[3] + pwArray[4] + pwArray[5] + pwArray[6] + pwArray[7] + pwArray[8] + pwArray[9];
+  
+// pwString test
+console.log("Password: " + pwString);
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-// for loop to determine pw size
-  for (var i = 0; i<10; i++) {
-    console.log("PW Character: " + pwCharacter[i]);
-  }
-  passwordText.value = password;
+  // variable to assign the pwArray to a single string
+  var pwString = pwArray[0] + pwArray[1] + pwArray[2] + pwArray[3] + pwArray[4] + pwArray[5] + pwArray[6] + pwArray[7] + pwArray[8] + pwArray[9];
+  
+  console.log(pwString);
+  
 
 }
 
