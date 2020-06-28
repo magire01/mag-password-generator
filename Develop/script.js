78936bae92f54c94ec609ebf6928d02fa5e71fa5
Mark Gire <magire01@gmail.com>
2020-06-28 17:18:01 -0400

    Created empty array pwString and a forloop to push 10 random characters to pwString

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 // pwCharacter is an array with all possible password character options
 var pwCharacter = "abcdeftghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()".split("");
-
-
+// pwString is an empty arry that will be filled with the for loop
+var pwString = []
+//pw Character test
 console.log(pwCharacter);
-for (var i = 0; i<10; i++) {
-  console.log("PW Character: " + pwCharacter[Math.floor(Math.random() * (Math.floor(72) - Math.ceil(0) + 1))]);
-}
 
+// for loop to push 10 random characters to pwString array
+for (var i = 0; i<10; i++) {
+  pwString.push(pwCharacter[Math.floor(Math.random() * (Math.floor(72) - Math.ceil(0) + 1))]);
+  
+
+  // console.log("PW Character: " + pwCharacter[Math.floor(Math.random() * (Math.floor(72) - Math.ceil(0) + 1))]);
+}
+console.log(pwString);
 
 // Write password to the #password input
 function writePassword() {
