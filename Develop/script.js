d79178ea4bca705ef610731b0b23915b937daa6f
Mark Gire <magire01@gmail.com>
2020-06-28 20:08:47 -0400

    Changed pwCharacter to an open array, created prompts for different character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// pwCharacter is an array with all possible password character options
-var pwCharacter = "abcdeftghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()".split("");
+
+// pwCharacter is an array with the selected password character options
+var pwCharacter = [];
+
+//generate groups of arrays for lowercase uppercase numbers and special characters
+var characterLower = "abcdefghijklmnopqrstuvwxyz".split("");
+var characterUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var characterNumber = "1234567890".split("");
+var characterSpecial ="!@#$%^&*()-+=}{~?><".split("");
+
+//create prompts for each group of character
+var lowerPrompt = confirm("Include Lower Case Letters?");
+var upperPrompt = confirm("Include Upper Case Letters?");
+var numberPrompt = confirm("Include Numbers?");
+var specialPrompt = confirm("Include Special Characters?");
+
+// Create if statement to push character types to the pwCharacter variable
+if (lowerPrompt == true) {
+  pwCharacter.push(characterLower);
+}
+
+// Character test
+console.log("Character test: " + pwCharacter);
+
 // pwArray is an empty arry that will be filled with the for loop
 var pwArray = []
 //pw Character test
@@ -14,10 +36,9 @@ var pwLength = prompt("How many characters do you want your password to be?");
   //pwLength test
 console.log("Length of Password: " + pwLength);
 
-  //pwLength
 
 function generatePassword() {
-  // for loop to push 10 random characters to pwArray
+  // for loop to push <pwLength> random characters to pwArray
   for (var i = 0; i<pwLength; i++) {
     //push 10 random characters to pwString array
     pwArray.push(pwCharacter[Math.floor(Math.random() * (Math.floor(72) - Math.ceil(0) + 1))]);
@@ -40,10 +61,10 @@ console.log("pwString Test: " + pwString)
 //Turn pwString array into single string
 var pwFull = pwString.join("");
 //pwFull test
-console.log(pwFull);
+console.log("pwFull Test: " + pwFull);
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   //writes pwString to the #password input
